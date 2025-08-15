7e2e109549d2142f93b7f17b6095f4697b14ac0d
wwreyti <147952458+wwreyti@users.noreply.github.com>
2025-08-15 03:18:15 +0300

    Update api1.php

--- api/api1.php
+++ api/api1.php
@@ -1,148 +1,148 @@
 {
 	"launcher_version": 55,
 	"launcher_url": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/launcher_space.apk?edgar=1829top09",
-	"launcher_path": "/storage/emulated/0/Android/data/ru.edgar.space/files/launcher_space.apk",
+	"launcher_path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/launcher_space.apk",
 	"launcher_name": "/launcher_space.apk",
 	"isTest": true,
 	"test_api": true,
         "api": "https://raw.githubusercontent.com/edgardevwork/space-json-php/main/api/main.json?edgar=1829top09",
 	"archives": [
         {
-            "type": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb",
-			"zip_path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/archive_txd1.zip",
+            "type": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb",
+			"zip_path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/archive_txd1.zip",
             "size": 545036062,
             "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_txd1.zip?edgar=1829top09",
 			"paths": [
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/gta_int"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/gta_int"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/gta3"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/gta3"
 				}
 			]
         },
 		{
-            "type": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb",
-			"zip_path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/archive_txd2.zip",
+            "type": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb",
+			"zip_path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/archive_txd2.zip",
             "size": 269290690,
             "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_txd2.zip?edgar=1829top09",
 			"paths": [
 				
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/gui"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/gui"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/menu"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/menu"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/mobile"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/mobile"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/player"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/player"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/radar"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/radar"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/samp"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/samp"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/txd"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/txd"
 				}
 			]
         },
 		{
-            "type": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb",
-			"zip_path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/archive_img1.zip",
+            "type": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb",
+			"zip_path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/archive_img1.zip",
             "size": 350976000,
             "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_img1.zip?edgar=1829top09",
 			"paths": [
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/gta_int.img"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/gta_int.img"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/player.img"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/player.img"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/samp.img"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/samp.img"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/SAMPCOL.img"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/SAMPCOL.img"
 				}
 			]
         },
 		{
-            "type": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb",
-			"zip_path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/archive_img2.zip",
+            "type": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb",
+			"zip_path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/archive_img2.zip",
             "size": 648009728,
             "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_img2.zip?edgar=1829top09",
 			"paths": [
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/texdb/gta3.img"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/texdb/gta3.img"
 				}
 			]
         },
         {
-            "type": "/storage/emulated/0/Android/data/ru.edgar.space/files",
-			"zip_path": "/storage/emulated/0/Android/data/ru.edgar.space/files/archive_lite.zip",
+            "type": "/storage/emulated/0/Android/data/ru.quin.mobile/files",
+			"zip_path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/archive_lite.zip",
             "size": 158215375,
             "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_lite.zip?edgar=1829top09",
 			"paths": [
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/data"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/data"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/models"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/models"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/SPACE"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/SPACE"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/anim"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/anim"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/audio"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/audio"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/fonts"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/fonts"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/loader"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/loader"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/Text"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/Text"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/Textures"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/Textures"
 				}
 			]
         },
         {
-            "type": "/storage/emulated/0/Android/data/ru.edgar.space/files",
-			"zip_path": "/storage/emulated/0/Android/data/ru.edgar.space/files/archive_config.zip",
+            "type": "/storage/emulated/0/Android/data/ru.quin.mobile/files",
+			"zip_path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/archive_config.zip",
             "size": 733232,
             "urls": "https://triomshors.ru/matrp-zakaz-edgar/files/build5/archive_config.zip?edgar=1829top09",
 			"paths": [
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/Adjustable.cfg"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/Adjustable.cfg"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/gta_sa.set"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/gta_sa.set"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/GTASAMP10.b"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/GTASAMP10.b"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/gtasatelem.set"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/gtasatelem.set"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/stream.ini"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/stream.ini"
 				},
 				{
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/version.txt"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/version.txt"
 				},
                                 {
-					"path": "/storage/emulated/0/Android/data/ru.edgar.space/files/CINFO.BIN"
+					"path": "/storage/emulated/0/Android/data/ru.quin.mobile/files/CINFO.BIN"
 				}
 			]
         }
